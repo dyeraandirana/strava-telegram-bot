c4eb9ca20795d073071dfd9001cc62b59c6f684a
dyeraandirana <135441436+dyeraandirana@users.noreply.github.com>
2025-08-30 06:09:39 +0700

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from strava_auth import exchange_code_for_token
+from sheets import save_user_token
+
+app = Flask(__name__)
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    user_id = request.args.get("state")  # bisa dikirim via Telegram
+    token_data = exchange_code_for_token(code)
+    save_user_token(user_id, token_data["access_token"], token_data["refresh_token"])
+    return "Strava berhasil terhubung!"
