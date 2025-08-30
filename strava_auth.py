3166d22a68f2980cda549a182b0cb53097f63b01
dyeraandirana <135441436+dyeraandirana@users.noreply.github.com>
2025-08-30 08:20:56 +0700

    Update strava_auth.py

--- strava_auth.py
+++ strava_auth.py
@@ -1,9 +1,17 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("STRAVA_REDIRECT_URI")
 
 def exchange_code_for_token(code):
     response = requests.post("https://www.strava.com/oauth/token", data={
-        "client_id": "YOUR_CLIENT_ID",
-        "client_secret": "YOUR_CLIENT_SECRET",
+        "client_id": STRAVA_CLIENT_ID,
+        "client_secret": STRAVA_CLIENT_SECRET,
         "code": code,
         "grant_type": "authorization_code"
     })
