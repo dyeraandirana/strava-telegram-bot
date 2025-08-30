4952f2293e8f5d03ddffe626237fe501062cdfca
dyeraandirana <135441436+dyeraandirana@users.noreply.github.com>
2025-08-30 08:20:23 +0700

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,12 +1,14 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 
 def get_sheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
-    return client.open("StravaBotData").sheet1
-
-def save_user_token(user_id, access_token, refresh_token):
-    sheet = get_sheet()
-    sheet.append_row([str(user_id), access_token, refresh_token])
+    return client.open_by_key(SPREADSHEET_ID).sheet1
